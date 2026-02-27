e5d28683e7d34d70e3f17d6a79250c82fc1e1e9c
Trung Nguyễn (Curly) <curly.saigonese@gmail.com>
2026-02-27 16:51:47 +0700

    feat(ui): add nuqs typed state factory

--- ui/web/src/hooks/use-url-state.ts
+++
@@ -1,7 +0,0 @@
-import { createSearchParams } from 'nuqs';
-
-export const [searchParams, setSearchParams, getParam] = createSearchParams({
-  // Default options
-  shallow: false, // useRouter.replace instead of push
-  window: typeof window !== 'undefined' ? window : undefined,
-});
