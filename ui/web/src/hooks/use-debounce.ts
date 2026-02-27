f94ee19d04170701f2d2db547b04f2e4472dec9f
Trung Nguyễn (Curly) <curly.saigonese@gmail.com>
2026-02-27 16:32:11 +0700

    refactor: use usehooks-ts useDebounce

--- ui/web/src/hooks/use-debounce.ts
+++ ui/web/src/hooks/use-debounce.ts
@@ -1,12 +1,1 @@
-import { useEffect, useState } from "react";
-
-export function useDebounce<T>(value: T, delayMs: number = 300): T {
-  const [debounced, setDebounced] = useState(value);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setDebounced(value), delayMs);
-    return () => clearTimeout(timer);
-  }, [value, delayMs]);
-
-  return debounced;
-}
+export { useDebounce } from "usehooks-ts";
